0869440f0115c1af745150638b253ac5f4c62d42
sunpanyu <996251389@qq.com>
2023-02-06 18:36:11 +0800

    [vite配置] 增加部分配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { createViteResolve } from "./config/resolve";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // 解析配置
+  resolve: createViteResolve(__dirname),
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://139.224.54.218:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
