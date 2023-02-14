8c4635dc2c79ca068f642fc71f3bb773ed92518e
sunpanyu <996251389@qq.com>
2023-02-14 09:47:51 +0800

    [首页模块} 增加描述

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,38 +1,13 @@
 /**
  * 首页
  */
-import { useSnapshot, proxy } from "valtio";
-import { openNewWindow } from "@/utils/windowUtils";
-import request from "@/utils/request";
-// import "./index.less";
-import classss from "./index.module.less";
-import { name } from "../../../package.json";
-console.log("name", name);
-
-// const state = proxy({
-//   routes: __APP_URL__,
-// });
+import "./index.less";
 
 const Home = () => {
-  //   const snap = useSnapshot(state);
   return (
-    <div className={classss.container}>
-      3333
-      <button
-        onClick={() => {
-          openNewWindow({ url: "https://www.baidu.com" });
-        }}
-      >
-        点击
-      </button>
-      <button
-        onClick={async () => {
-          const response = await request({ url: "/api/crawling/list?id=1" });
-          console.log(response);
-        }}
-      >
-        请求数据
-      </button>
+    <div>
+      <h1>Welcome to React + Vite</h1>
+      <p>This is a example of a React + Vite application.</p>
     </div>
   );
 };
